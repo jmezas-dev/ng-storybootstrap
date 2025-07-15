8330029d183fe1f76ff23743781cb1ca5dd7ee1d
Jorge Meza <jmezas@mcllent.com>
2025-07-15 16:53:39 -0600

    + Added interfaces and functions to interact with some items in table.html and table.component.ts

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,17 +1,54 @@
-import { Component, Input, Output,EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
+import {IVacantRow} from '../../interfaces/IVacantRow';
 
 @Component({
-    selector: 'app-table',
-    templateUrl: './table.html',
-    styleUrls: ['./table.css'],
-  }
-)
+  selector: 'app-table',
+  templateUrl: './table.html',
+  styleUrls: ['./table.css']
+})
 
 export default class TableComponent {
-  /*
-   * The shape of the task object
-   */
-  @Input() task: any;
+  public vacants : IVacantRow[];
 
-  @Output() onTaskClick = new EventEmitter<any>();
+  constructor() {
+    this.vacants = [
+      {
+        id:1,
+        ticketNumber:"Ticket #123",
+        vacantName:"Desarrollador",
+        descriptionLink:"link",
+        recruiter:"Beatriz Pinzon",
+        status:"Activo",
+        recruiterStatus:"Asignada",
+        comments:"lorem ipsum dolor sit amet consectetur adipisicing elit."
+      },
+      {
+        id:2,
+        ticketNumber:"Ticket #124",
+        vacantName:"Desarrollado",
+        descriptionLink:"link",
+        recruiter:"Nicolas Mora",
+        status:"Activo",
+        recruiterStatus:"Asignada",
+        comments:"lorem ipsum dolor sit amet consectetur adipisicing elit."
+      }
+    ]
+  }
+
+  updateVacantRecruiter(newVacantRecruiter:string,vacantId:number ){
+    //TODO: REPLACE THIS CODE FOR A LOGIC THAT CONSUMES A SERVICE TO UPDATE THE FIELD
+    let foundVacant = this.vacants.filter(vacant => vacant.id === vacantId);
+    foundVacant[0].recruiter = newVacantRecruiter;
+  }
+
+  updateVacantStatus(newVacantStatus:string,vacantId:number) {
+    //TODO: REPLACE THIS CODE FOR A LOGIC THAT CONSUMES A SERVICE TO UPDATE THE FIELD
+    let foundVacant = this.vacants.filter(vacant => vacant.id === vacantId);
+    foundVacant[0].recruiterStatus = newVacantStatus;
+  }
 }
+
+
+/* HELP!
+*  I was kidnapped and the boss is forcing me
+*  to use angular and bootstrap 💀 */
---
+++ src/app/interfaces/IVacantRow.ts
@@ -0,0 +1,10 @@
+export interface IVacantRow {
+  id:number;
+  ticketNumber:string;
+  vacantName:string;
+  descriptionLink:string;
+  recruiter:string;
+  status:string;
+  recruiterStatus:string;
+  comments:string;
+}
