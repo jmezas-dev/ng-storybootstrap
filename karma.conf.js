0883e1285b427c4d0c90f3c0a1e084886cd2024c
Jorge Meza <jmezas@mcllent.com>
2025-07-25 17:25:20 -0600

    * testing angular tests in Jenkinsfile

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 module.exports = function(config) {
   config.set({
-    browsers: ['ChromeHeadlessCI'],
+    browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
