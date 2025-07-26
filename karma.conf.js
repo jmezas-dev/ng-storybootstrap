e0c3505264ce2a78397802bcb0973dcbca81e0af
Jorge Meza <jmezas@mcllent.com>
2025-07-25 18:51:59 -0600

    * Fixed tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,29 @@
 module.exports = function(config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        // you can add special configurations here if needed
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessCI: {
@@ -13,6 +37,11 @@ module.exports = function(config) {
         ]
       }
     },
-    // ... rest of your karma config
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
